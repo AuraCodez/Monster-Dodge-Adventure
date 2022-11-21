b3cc2146f757504afc3d3ec4c689889ba3d77cf2
Ryan L <fartman553@gmail.com>
2022-11-21 01:45:51 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,30 +20,38 @@ velocity = 1
 angle = 0
 clock = pygame.time.Clock()
 
+moveRight = False
+moveLeft = False
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x-= 25
+                moveLeft = True
             if event.key == pygame.K_RIGHT:
-                x += 25
+                moveRight = True
 
-
-
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moveLeft = False
+            if event.key == pygame.K_RIGHT:
+                moveRight = False
 
         if event.type == pygame.QUIT:
            exit()
 
+
+    if moveRight:
+        x+= 5
+    if moveLeft:
+        x-= 5
+
     window.fill((0,204,204))
     window.blit(pygame.transform.scale(ball,(150, 150)), (0, 0)) 
-
-
     window.blit(robot, (x,y))
     window.blit(ball,(600-width/2,360-height/2)) #Puts the ball at the middle of the screen, the centre of the window is at half its width and height
     pygame.display.flip() # After the window is filled with colour the image is drawn at the given location with the blit method. Then the contents of the window are updated with the function pygame.display.flip.
-
-
-
 
     clock.tick(60) #60 dictates that the loop should be executed 60 times a second
 
