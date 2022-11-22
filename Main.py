b1703e65daf8dd7842e7fdec63643da6d1c69ca0
Ryan L <fartman553@gmail.com>
2022-11-22 00:57:26 -0500

    Remove Unecessary objects

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import pygame #imports the pygame library into usage
 import math
+import random
 
 pygame.init() #initializes the pygame modules
 window = pygame.display.set_mode((1200, 720)) #creates a window with the function
@@ -7,6 +8,9 @@ window = pygame.display.set_mode((1200, 720)) #creates a window with the functio
 
 ball = pygame.image.load("ball.png")
 robot = pygame.image.load("robot.png")
+ball_speed = 5
+ball_velocity = 0
+ball_position = (900,5)
 
 x = 0
 y = 720-robot.get_height()
@@ -47,10 +51,13 @@ while True:
     if moveLeft:
         x-= 5
 
+
+
     window.fill((0,204,204))
     window.blit(pygame.transform.scale(ball,(150, 150)), (0, 0)) 
-    window.blit(robot, (x,y))
-    window.blit(ball,(600-width/2,360-height/2)) #Puts the ball at the middle of the screen, the centre of the window is at half its width and height
+    window.blit(pygame.transform.scale(ball,(150,150)),ball_position)
+    window.blit(robot,(x,y))
+    #window.blit(ball,(600-width/2,360-height/2)) #Puts the ball at the middle of the screen, the centre of the window is at half its width and height
     pygame.display.flip() # After the window is filled with colour the image is drawn at the given location with the blit method. Then the contents of the window are updated with the function pygame.display.flip.
 
     clock.tick(60) #60 dictates that the loop should be executed 60 times a second
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-    window.blit(robot, (0, 50))
+
